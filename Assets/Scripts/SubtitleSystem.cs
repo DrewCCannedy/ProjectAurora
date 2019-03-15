3fd1877de58a84951a6d590139d875b037886c35
Cannedy <dxc142830@utdallas.edu>
2019-03-15 15:21:24 -0500

    AI audio system added

--- Assets/Scripts/SubtitleSystem.cs
+++ Assets/Scripts/SubtitleSystem.cs
@@ -17,6 +17,14 @@ public class SubtitleSystem : MonoBehaviour
     // keeping track of which subtitles to play
     private int subtitleCount = 0;
 
+    // audio
+    [System.Serializable]
+    public class AudioClips {
+        public AudioClip[] clips;
+    }
+    public AudioClips[] audioClips;
+    public AudioSource audioSource;    
+
     // section variables
     private bool playIntro = true;
     private bool playSpaceSuit = false;
@@ -35,11 +43,10 @@ public class SubtitleSystem : MonoBehaviour
         subtitles.Add("AI: Oh good, you’re finally awake.");
         subtitles.Add("AI: I got bored while you were asleep and turned off auto pilot,");
         subtitles.Add("AI: but turns out a disembodied intercom system can’t pilot the ship.");
-        subtitles.Add("AI: Sadly, we've struck an asteroid.");
-        subtitles.Add("AI: You’re going to have to upload your research and get to an escape pod.");
-        subtitles.Add("AI: I believe you left your flashlight on your desk.");
-        subtitles.Add("AI: You never clean up after yourself.");
-        subtitles.Add("AI: You’ll need it. The power’s out and I know you’re afraid of the dark.");
+        subtitles.Add("AI: We've struck an asteroid and you’re going to have to upload your research and get to an escape pod.");
+        // subtitles.Add("AI: I believe you left your flashlight on your desk.");
+        // subtitles.Add("AI: You never clean up after yourself.");
+        // subtitles.Add("AI: You’ll need it. The power’s out and I know you’re afraid of the dark.");
         subtitleSections.Add(subtitles);
 
         // SpaceSuit Text [1]
@@ -48,8 +55,8 @@ public class SubtitleSystem : MonoBehaviour
         subtitles.Add("AI: Bad news: You didn’t change out the oxygen tank after your last mission.");
         subtitles.Add("AI: Oxygen levels are at ten percent.");
         subtitles.Add("AI: I’d give you about six minutes till you run out.");
-        subtitles.Add("AI: Nice knowing you, Aurora.");
-        subtitles.Add("AI: Since the door systems are offline you’re gonna have to use the manual lever.");
+        subtitles.Add("AI: Nice knowing you, Captain.");
+        // subtitles.Add("AI: Since the door systems are offline you’re gonna have to use the manual lever.");
         subtitleSections.Add(subtitles);
     }
 
@@ -57,14 +64,33 @@ public class SubtitleSystem : MonoBehaviour
     void Update()
     {
         if (playIntro) {
-            playIntro = PlaySubtitleSection(0);
+            playIntro = PlayAudioSection(0);
         } else if (playSpaceSuit) {
-            playSpaceSuit = PlaySubtitleSection(1);
+            playSpaceSuit = PlayAudioSection(1);
         } else {
             text.text = "";
         }
     }
 
+    bool PlayAudioSection(int section) {
+        if (!audioSource.isPlaying) {
+            // if all clips are played, move on
+            if (subtitleCount >= subtitleSections[section].Count) {
+                subtitleCount = 0;
+                panel.gameObject.SetActive(false);
+                return false;
+            }
+            panel.gameObject.SetActive(true);
+            // set subtitle text
+            text.text = subtitleSections[section][subtitleCount];
+            // play next audio
+            audioSource.PlayOneShot(audioClips[section].clips[subtitleCount]);
+            subtitleCount++;
+        }
+        return true;
+    }    
+
+    // Use this without audio
     // Play a subtitle section, will return true if it runs out of subtitles
     bool PlaySubtitleSection(int section) {
         // run every interval number of seconds
