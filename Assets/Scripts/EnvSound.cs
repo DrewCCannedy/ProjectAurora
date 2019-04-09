6209019f8bbd42e4e4f12a5472a1b2a6fded753e
Whitehead <njw150230@utdallas.edu>
2019-04-08 20:47:48 -0500

    ambience/colliders added

---
+++ Assets/Scripts/EnvSound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// made by drew, copied over by noah for other sounds
+public class EnvSound : MonoBehaviour
+{
+    AudioSource source;
+    public AudioClip clip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        StartCoroutine(PlaySoundAfterSeconds());
+    }
+
+    IEnumerator PlaySoundAfterSeconds()
+    {
+        while (true)
+        {
+            source.PlayOneShot(clip);
+            yield return new WaitForSeconds(5);
+        }
+    }
+}
