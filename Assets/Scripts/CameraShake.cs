62727adc8d1edf9b25bf15bafa07362dd37ab461
Whitehead <njw150230@utdallas.edu>
2019-05-08 22:43:14 -0500

    added shake sound

--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -8,6 +8,8 @@ public class CameraShake : MonoBehaviour
     // if null.
     public Transform camTransform;
 
+    // shake sound
+    public AudioClip crash;
 
     // How long the object should shake for.
     public float shakeDuration = 0f;
@@ -16,7 +18,7 @@ public class CameraShake : MonoBehaviour
     public float shakeAmount = 0.7f;
     public float decreaseFactor = 1.0f;
     public int timeSinceShake = 0;
-    
+    private bool soundPlayed = false;
 
     Vector3 originalPos;
 
@@ -35,21 +37,28 @@ public class CameraShake : MonoBehaviour
 
     void Update()
     {
-        if (timeSinceShake > 600)
+        if (timeSinceShake > 1000)
         {
+            
             if (shakeDuration > 0)
             {
+                
                 camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
-
+                if(!soundPlayed){
+                    GetComponent<AudioSource>().PlayOneShot(crash);
+                }
+                soundPlayed = true;
                 shakeDuration -= Time.deltaTime * decreaseFactor;
             }
             else
             {
                 shakeDuration = 1f;
+                soundPlayed = false;
                 camTransform.localPosition = originalPos;
                 timeSinceShake = 0;
             }
         }
+
         timeSinceShake++;
     }
 
